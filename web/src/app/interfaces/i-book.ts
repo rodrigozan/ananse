834391aea75dd2fd5a1e66533aa5579eada78078
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2025-02-11 16:38:39 -0300

    ➕ Created IBook interface

---
+++ web/src/app/interfaces/i-book.ts
@@ -0,0 +1,24 @@
+export interface IBook {
+    title: string;
+    author: string;
+    shortDescription: string;
+    summary: string;
+    coverImage: string;
+    centralConflict: string;
+    characters: {
+        protagonists: string[];
+        mainCharacters: string[];
+        secondaryCharacters: string[];
+    };
+    literaryStructure: {
+        act1: string;
+        act2a: string;
+        act2b: string;
+        act3: string;
+        plotPoints: string[];
+    };
+    scenes: {
+        oddScenes: string[];
+        evenScenes: string[];
+    };
+}
