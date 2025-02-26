9cc2a47929e64391ccec9f8a288c14e087e8bb38
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2025-02-26 14:34:12 -0300

    🛠️ Update (Route): Index

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,1 +1,18 @@
+import { AuthMiddleware } from '../middlewares/AuthMiddleware';
+
+const authMiddleware = new AuthMiddleware();
+
+const authenticate = authMiddleware.authenticate.bind(authMiddleware);
+
+export { default as auth } from './AuthRoute'
 export { default as user } from './UserRoute'
+export { default as character } from './CharacterRoute'
+export { default as event } from './EventRoute'
+export { default as scenario } from './ScenarioRoute'
+export { default as serie } from './SerieRoute'
+export { default as book } from './BookRoute'
+
+
+
+export { authenticate };
+
